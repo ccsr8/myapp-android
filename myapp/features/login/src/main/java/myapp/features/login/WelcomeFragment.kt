1107780bea51ef171758b6fb3fa80f61e89dbaed
chatchai.s <chatchai.s@goldenland.co.th>
2019-07-19 08:36:24 +0700

    Added  - fragment to login flow

--- myapp/features/login/src/main/java/myapp/features/login/WelcomeFragment.kt
+++ myapp/features/login/src/main/java/myapp/features/login/WelcomeFragment.kt
@@ -5,18 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
 class WelcomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+    //region [event handlers]
 
-        return view
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        welcomeNextButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
+        }
+    }
+
+    //endregion
 
 }
