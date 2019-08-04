b47b078de3f411fdfe8ec9dd83c23d8b435badf1
chatchai <chatchai4566@gmail.com>
2019-08-04 21:56:34 +0700

    Create - Appdb feature

---
+++ myapp/libraries/appdb/src/main/java/myapp/libraries/appdb/AppdbMainActivity.kt
@@ -0,0 +1,12 @@
+package myapp.libraries.appdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppdbMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appdb_main)
+    }
+}
