78ad3555c67f716ebfc044fb654da419427db3e5
chatchai <chatchai4566@gmail.com>
2019-08-13 22:39:56 +0700

    Created - repository

---
+++ myapp/features/arch_room/src/main/java/myapp/features/arch_room/db/WordRoomDatabase.kt
@@ -0,0 +1,36 @@
+package myapp.features.arch_room.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): RoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "Word_database"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
