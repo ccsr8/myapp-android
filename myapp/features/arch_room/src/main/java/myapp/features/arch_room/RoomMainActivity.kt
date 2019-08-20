074a22ba65e2f06f8c23e0c7fa351f4a72c883c7
chatchai.s <chatchai.s@goldenland.co.th>
2019-08-20 09:07:12 +0700

    Edited -refactor code

--- myapp/features/arch_room/src/main/java/myapp/features/arch_room/RoomMainActivity.kt
+++ myapp/features/arch_room/src/main/java/myapp/features/arch_room/RoomMainActivity.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_room_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import myapp.features.arch_room.db.Word
 
 class RoomMainActivity : AppCompatActivity() {
@@ -27,20 +28,10 @@ class RoomMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycleView)
         val adapter = WordListAdapter(this)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
-        wordViewModel.allWords.observe(this, Observer { words ->
-            words?.let { adapter.setWords(it) }
-        })
-
-        fab.setOnClickListener {
-            val intent = Intent(this@RoomMainActivity, RoomNewWordActivity::class.java)
-            startActivityForResult(intent, newWordActivityRequestCode)
-        }
+        this.setViewModel(adapter)
+        this.setControl(adapter)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -55,6 +46,27 @@ class RoomMainActivity : AppCompatActivity() {
             }
         } else {
             Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //endregion
+
+    //region [private members]
+
+    private fun setViewModel(adapter: WordListAdapter) {
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            words?.let { adapter.setWords(it) }
+        })
+    }
+
+    private fun setControl(adapter: WordListAdapter) {
+        recycleView.adapter = adapter
+        recycleView.layoutManager = LinearLayoutManager(this)
+
+        fab.setOnClickListener {
+            val intent = Intent(this@RoomMainActivity, RoomNewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
         }
     }
 
