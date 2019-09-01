7d1cf2636e4200b092d472fe6444128311473ca2
chatchai <chatchai4566@gmail.com>
2019-09-01 23:35:18 +0700

    Created - add tasks feature

--- module-by-package/app/src/main/java/com/example/appbypackage/data/AppDatabase.kt
+++ module-by-package/app/src/main/java/com/example/appbypackage/data/AppDatabase.kt
@@ -1,4 +1,13 @@
 package com.example.appbypackage.data
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.appbypackage.features.tasks.data.local.TasksDao
+import com.example.appbypackage.features.tasks.data.local.TaskEntity
+
+@Database(entities = [TaskEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TasksDao
+
 }
---
+++ module-by-package/app/src/main/java/com/example/appbypackage/features/tasks/test.kt
@@ -0,0 +1,2 @@
+package com.example.appbypackage.features.tasks
+
