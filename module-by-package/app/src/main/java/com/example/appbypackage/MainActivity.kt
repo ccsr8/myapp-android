7a6e3c2fe9855e323d90669ab4eee4baca228dae
chatchai <chatchai4566@gmail.com>
2019-09-18 21:56:06 +0700

    Edited - add app bar

--- module-by-package/app/src/main/java/com/example/appbypackage/MainActivity.kt
+++ module-by-package/app/src/main/java/com/example/appbypackage/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.appbypackage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavigationDrawer()
+        setSupportActionBar(toolbar)
+
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.tasksFragment)
+            .setDrawerLayout(drawerLayout)
+            .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+    }
+
+    private fun setupNavigationDrawer() {
+        drawerLayout = drawer_layout.apply {
+            setStatusBarBackground(R.color.colorPrimaryDark)
+        }
     }
 }
