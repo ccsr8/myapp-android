d7d076042ad2ac6ca61fea4fd86658937db215a0
chatchai.s <chatchai.s@goldenland.co.th>
2019-09-19 09:03:21 +0700

    Created - add statistics fragment

--- module-by-package/app/src/main/java/com/example/appbypackage/MainActivity.kt
+++ module-by-package/app/src/main/java/com/example/appbypackage/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //region [private members]
+
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    //endregion
+
+    //region [event handlers]
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,16 +30,28 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val navController: NavController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration.Builder(R.id.tasksFragment)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.tasksFragment, R.id.statisticsFragment)
             .setDrawerLayout(drawerLayout)
             .build()
         setupActionBarWithNavController(navController, appBarConfiguration)
         nav_view.setupWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    //endregion
+
+    //region [private members]
+
     private fun setupNavigationDrawer() {
         drawerLayout = drawer_layout.apply {
             setStatusBarBackground(R.color.colorPrimaryDark)
         }
     }
+
+    //endregion
 }
