8e440545ce7cf2561e0ffc617e0f7aa4a98f7408
chatchai <chatchai4566@gmail.com>
2019-09-24 22:02:51 +0700

    Created - add view model

--- module-by-package/app/src/main/java/com/example/appbypackage/features/tasks/TasksFragment.kt
+++ module-by-package/app/src/main/java/com/example/appbypackage/features/tasks/TasksFragment.kt
@@ -6,13 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 
 import com.example.appbypackage.R
+import com.example.appbypackage.databinding.FragmentTasksBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class TasksFragment : Fragment() {
+
+    private val viewModel by viewModels<TasksViewModel>()
+    // private val args: TasksFragmentArgs by navArgs()
+    private lateinit var viewDataBinding: FragmentTasksBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
